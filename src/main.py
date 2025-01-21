b31f40f4fbbf17eb60557b1a7ec3c42dc9ffde54
JustinCO2 <cgetlifted@gmail.com>
2025-01-21 13:52:40 -0500

    asd

--- src/main.py
+++ src/main.py
@@ -40,7 +40,6 @@ guild_obj = discord.Object(id=guild_id)
 @bot.event
 async def on_ready():
     try:
-        await bot.tree.clear_commands(guild=guild_obj)
         synced = await bot.tree.sync(guild=guild_obj)
         print(f"Cleared and synced {len(synced)} command(s).")
         for command in bot.tree.get_commands():
