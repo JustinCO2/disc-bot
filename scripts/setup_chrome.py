2bffd16645e2c3d572e3f20ab8d9e7d664e36a84
JustinCO2 <cgetlifted@gmail.com>
2025-01-27 20:22:11 -0500

    please work

--- scripts/setup_chrome.py
+++ scripts/setup_chrome.py
@@ -1,5 +1,9 @@
-import sys, os
+import sys
+import os
+
+# Ensure `/app` is in Python's search path so `utils` is importable
 sys.path.insert(0, os.getcwd())
+
 from utils.setup_chrome_testing_binaries import setup_chrome_testing_binaries
 
 def main():
