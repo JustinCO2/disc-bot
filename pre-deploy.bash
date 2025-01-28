7978629d0e7751c8162046cd1e0967b8eaedce7c
Vivek Bose <waivek@gmail.com>
2025-01-28 08:22:57 +0530

    Attempt v06

--- pre-deploy.bash
+++ pre-deploy.bash
@@ -1,5 +1,5 @@
 sudo apt-get update
 xargs -a dependencies.txt sudo apt-get install -y
 python src/utils/setup_chrome_testing_binaries.py
-chmod +x bin/chromedriver
-chmod +x bin/chrome-headless-shell-linux64/chrome-headless-shell
+# chmod +x bin/chromedriver
+# chmod +x bin/chrome-headless-shell-linux64/chrome-headless-shell
